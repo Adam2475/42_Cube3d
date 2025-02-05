a6c37cf68266ca61c32f4a75cedbf99bf5f2e973
Ju3ex <giuliorindi@gmail.com>
2025-02-05 17:41:57 +0100

    fixed parsing

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,11 +6,32 @@
 /*   By: girindi <girindi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 13:15:31 by adapassa          #+#    #+#             */
-/*   Updated: 2025/02/05 15:47:34 by girindi          ###   ########.fr       */
+/*   Updated: 2025/02/05 17:23:40 by girindi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cube3d.h"
+
+
+// int check_valid_texture(char **matrix)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (matrix[i]) 
+// 	{
+// 	    if (strncmp(matrix[i], "NO", 2) != 0 &&
+// 	        strncmp(matrix[i], "SO", 2) != 0 &&
+// 	        strncmp(matrix[i], "EA", 2) != 0 &&
+// 	        strncmp(matrix[i], "WE", 2) != 0 &&
+// 			strncmp(matrix[i], "F", 1) != 0 &&
+// 			strncmp(matrix[i], "C", 1) != 0
+// 			)
+// 	        	return (1); // Errore trovato
+// 		i++;
+// 	    }
+// 	return (0); // Tutti i valori sono validi
+// }
 
 int	map_parsing(char **av, t_map *map)
 {
@@ -97,8 +118,14 @@ int	get_textures(char **tmp, t_map *map)
 	j = 0;
 	while (tmp[++i])
 	{
-		if (!in_texture(tmp[i]))
+		if (*tmp[i] == '\n')
+			continue ;
+		else if (!in_texture(tmp[i]))
 			check++;
+		else if (check == 6)
+			break ;
+		else
+			return (1);
 	}
 	if (check != 6)
 		return (1);
